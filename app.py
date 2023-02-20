defa8d27bc9727f1de71570e58faad9bb343a897
beercafeguy <hemchandra@outlook.com>
2023-02-20 21:20:08 +0530

    Created a simple API with flask

--- app.py
+++ app.py
@@ -1,57 +1,58 @@
-from flask import Flask,request ,render_template , jsonify
-
+from flask import Flask,render_template,redirect,request,jsonify
+import math
 app = Flask(__name__)
 
-
-@app.route('/')
+@app.route("/",methods=['GET','POST'])
 def home_page():
     return render_template('index.html')
 
-
-@app.route('/math',methods=['POST'])
-def math_ops():
-    if(request.method == 'POST'):
-        ops = request.form['operation']
+@app.route("/math",methods=['POST'])
+def math_operation():
+    if request.method == "POST":
+        operation = request.form['operation']
         num1 = int(request.form['num1'])
         num2 = int(request.form['num2'])
-        if ops == 'add':
-            r = num1+num2
-            result = "The sum of " + str(num1) + 'and ' + str(num2) + "is " + str(r)
-        if ops == 'subtract':
-            r = num1-num2
-            result = "The subtract of " + str(num1) + 'and ' + str(num2) + "is " + str(r)
-        if ops == 'multiply':
-            r = num1*num2
-            result = "The multiply of " + str(num1) + 'and ' + str(num2) + "is " + str(r)
-        if ops == 'divide':
-            r = num1/num2
-            result = "The divide of " + str(num1) + 'and ' + str(num2) + "is " + str(r)
-            
-        return render_template('results.html' , result = result)
+        result = 0
+        if operation == "add":
+            result_tmp = num1+num2
+            result = "Sum of {} and {} is {}".format(num1,num2,result_tmp)
+        if operation == "subtract":
+            result_tmp = num1 - num2
+            result = "Subtraction of {} and {} is {}".format(num1,num2,result_tmp)
+        if operation == "multiply":
+            result_tmp = num1*num2
+            result = "Multiplication of {} and {} is {}".format(num1,num2,result_tmp)
+        if operation == "divide":
+            result_tmp = num1/num2
+            result = "Division of {} and {} is {}".format(num1,num2,result_tmp)
+        if operation == "log":
+            result_tmp = math.log(num1,num2)
+            result = "LOG of {} and {} is {}".format(num1,num2,result_tmp)
+        return render_template("results.html",result=result)    
 
-
-
-
-@app.route('/postman_action',methods=['POST'])
-def math_ops1():
-    if(request.method == 'POST'):
-        ops = request.json['operation']
+@app.route("/math_postman",methods=['POST'])
+def math_operation_postman():
+    if request.method == "POST":
+        operation = request.json['operation']
         num1 = int(request.json['num1'])
         num2 = int(request.json['num2'])
-        if ops == 'add':
-            r = num1+num2
-            result = "The sum of " + str(num1) + 'and ' + str(num2) + "is " + str(r)
-        if ops == 'subtract':
-            r = num1-num2
-            result = "The subtract of " + str(num1) + 'and ' + str(num2) + "is " + str(r)
-        if ops == 'multiply':
-            r = num1*num2
-            result = "The multiply of " + str(num1) + 'and ' + str(num2) + "is " + str(r)
-        if ops == 'divide':
-            r = num1/num2
-            result = "The divide of " + str(num1) + 'and ' + str(num2) + "is " + str(r)
-            
-        return jsonify(result)
+        result = 0
+        if operation == "add":
+            result_tmp = num1+num2
+            result = "Sum of {} and {} is {}".format(num1,num2,result_tmp)
+        if operation == "subtract":
+            result_tmp = num1 - num2
+            result = "Subtraction of {} and {} is {}".format(num1,num2,result_tmp)
+        if operation == "multiply":
+            result_tmp = num1*num2
+            result = "Multiplication of {} and {} is {}".format(num1,num2,result_tmp)
+        if operation == "divide":
+            result_tmp = num1/num2
+            result = "Division of {} and {} is {}".format(num1,num2,result_tmp)
+        if operation == "log":
+            result_tmp = math.log(num1,num2)
+            result = "LOG of {} and {} is {}".format(num1,num2,result_tmp)
+        return jsonify({'result':result})   
 
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(host="0.0.0.0")
